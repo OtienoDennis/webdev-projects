6355f2085ebbda49f65adcbef9290d5352382fbf
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-22 23:52:29 +0300

    Create SearchBar Component

--- frontend-projects/travel-planner-app/src/App.jsx
+++ frontend-projects/travel-planner-app/src/App.jsx
@@ -1,8 +1,11 @@
+import SearchBar from './components/SearchBar';
+
 function App() {
 
   return (
-    <div className='w-[calc(100vw-40px)] h-[calc(100vh-40px)] bg-sky-400 mx-auto my-5 rounded-md'>
+    <div className='w-[calc(100vw-40px)] h-[calc(100vh-40px)] bg-sky-400 mx-auto my-5 rounded-md flex flex-col items-center gap-5'>
       <h1 className='text-blue-900 text-center pt-8 text-2xl font-bold font-sans'>TRAVEL PLANNER</h1>
+      <SearchBar />
     </div>
   );
 }
--- frontend-projects/travel-planner-app/src/components/SearchBar.jsx
+++ frontend-projects/travel-planner-app/src/components/SearchBar.jsx
@@ -1,5 +1,19 @@
 export default function SearchBar() {
   return (
-    <div>SearchBar</div>
-  )
+    <div className='bg-[#bb8686] flex gap-4 p-5 rounded-md'>
+      <button className='text-white font-bold bg-black p-1 rounded-md'>
+        SEARCH
+      </button>
+      <input
+        type='text'
+        placeholder='CITY'
+        className='p-1 rounded-md outline-none'
+      />
+      <input
+        type='text'
+        placeholder='KEYWORD'
+        className='p-1 rounded-md outline-none'
+      />
+    </div>
+  );
 }
