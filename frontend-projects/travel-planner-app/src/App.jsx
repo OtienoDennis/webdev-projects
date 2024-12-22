351feb2820e642d0bdc8eceba6a4a9ff068627c3
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-22 23:38:57 +0300

    Add background Color to App Component

--- frontend-projects/travel-planner-app/src/App.jsx
+++ frontend-projects/travel-planner-app/src/App.jsx
@@ -1,12 +1,10 @@
 function App() {
 
   return (
-    <>
-      <p className="underline">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='w-[calc(100vw-40px)] h-[calc(100vh-40px)] bg-sky-400 mx-auto my-5 rounded-md'>
+      <h1 className='text-blue-900 text-center pt-8 text-2xl font-bold font-sans'>TRAVEL PLANNER</h1>
+    </div>
+  );
 }
 
 export default App
