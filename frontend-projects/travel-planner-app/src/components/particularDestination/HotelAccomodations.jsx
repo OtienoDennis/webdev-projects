4bbf2dbb7e13aca55f16a3977e0528ce2754670a
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-29 19:50:51 +0300

    Add HotelAccommodations component for fetching and displaying hotel offers based on city code

---
+++ frontend-projects/travel-planner-app/src/components/particularDestination/HotelAccomodations.jsx
@@ -0,0 +1,63 @@
+import useFetchDataApi from '../../fetchingFunctions/useFetchDataApi';
+import ErrorComponent from '../ErrorComponent';
+
+export default function HotelAccomodations(cityCode, dispatch) {
+  const isValidParameters = cityCode;
+  const URLHOTELOFFERS = `https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode=`;
+
+  // FETCHING HOTEL OFFERS
+  const {
+    data: hotelData,
+    error: hotelError,
+    loading: hotelLoading,
+  } = useFetchDataApi(
+    URLHOTELOFFERS,
+    isValidParameters ? cityCode : '',
+    dispatch,
+    {
+      dataActionType: 'hotelOffers',
+      loadingActionType: 'setLoadingState',
+    }
+  );
+
+  if (!isValidParameters) {
+    return <ErrorComponent errortext='Invalid parameters provided.' />;
+  }
+
+  if (hotelLoading) {
+    return (
+      <h1 className='text-2xl text-white font-extrabold text-center'>
+        Loading...
+      </h1>
+    );
+  }
+
+  if (hotelError) {
+    return <ErrorComponent errortext={hotelError} />;
+  }
+  return (
+    <div className='border-slate-300 border-2 rounded-md p-2 bg-slate-400 my-2'>
+      <h3 className='text-center text-gray-200 font-extrabold underline'>
+        Hotel Accomodations
+      </h3>
+      <div className='border-slate-300 border-2 rounded-md p-2 bg-slate-400 my-2 '>
+        <ul className='flex justify-between '>
+          <li className='text-sm font-semibold text-gray-300'>One</li>
+          <li className='text-sm font-semibold text-gray-300'>num</li>
+        </ul>
+        <ul className='flex justify-between '>
+          <li className='text-sm font-semibold text-gray-300'>One</li>
+          <li className='text-sm font-semibold text-gray-300'>num</li>
+        </ul>
+        <ul className='flex justify-between '>
+          <li className='text-sm font-semibold text-gray-300'>One</li>
+          <li className='text-sm font-semibold text-gray-300'>num</li>
+        </ul>
+        <ul className='flex justify-between '>
+          <li className='text-sm font-semibold text-gray-300'>One</li>
+          <li className='text-sm font-semibold text-gray-300'>num</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
