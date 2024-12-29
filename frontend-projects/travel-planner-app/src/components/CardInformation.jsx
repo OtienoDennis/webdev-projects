3581325b8ae361baf3f453af313b334ef3b50102
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-29 18:04:05 +0300

    Remove commented code and clean up CardInformation component

--- frontend-projects/travel-planner-app/src/components/CardInformation.jsx
+++ frontend-projects/travel-planner-app/src/components/CardInformation.jsx
@@ -1,13 +1,8 @@
 import { Link } from 'react-router-dom';
 
 export default function CardInformation({ city, cityCode , country, latitude, longitude}) {
-  // const regionNamesInEnglish = new Intl.DisplayNames(['en'], {
-  //   type: 'region',
-  // });
-  // const country = regionNamesInEnglish.of(`${cityCode}`);
-  
+
   return (
-    // `/destination/${city}` is the path to the destination page
     <Link to={`/destination/${city}/${cityCode}/${latitude}/${longitude}`}>
       <div className='flex justify-between mt-3 bg-slate-400 p-2 rounded hover:scale-105 hover:cursor-pointer transition ease-in-out duration-[25]'>
         <p className='text-white text-sm'>{city}</p>
