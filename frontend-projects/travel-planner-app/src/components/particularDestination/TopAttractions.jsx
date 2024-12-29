e767665d740dd479571a935aaf0efb6342051a6b
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-29 19:51:01 +0300

    Add TopAttractions component for fetching and displaying top attractions based on latitude and longitude

---
+++ frontend-projects/travel-planner-app/src/components/particularDestination/TopAttractions.jsx
@@ -0,0 +1,69 @@
+import useFetchDataApi from '../../fetchingFunctions/useFetchDataApi';
+import ErrorComponent from '../ErrorComponent';
+
+export default function TopAttractions({ latitude, longitude, dispatch }) {
+  const isValidParameters = latitude && longitude;
+
+  const URLTOPATTRACTIONS = `https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=${latitude}&longitude=`;
+
+  // FETCHING TOP ATTRACTION SITES
+  const {
+    data: attractionsSiteData,
+    error: attractionsSiteError,
+    loading: attractionsSiteLoading,
+  } = useFetchDataApi(
+    URLTOPATTRACTIONS,
+    isValidParameters ? longitude : '',
+    dispatch,
+    {
+      dataActionType: 'topAttractions',
+      loadingActionType: 'setLoadingState',
+    }
+  );
+
+   if (!isValidParameters) {
+      return <ErrorComponent errortext='Invalid parameters provided.' />;
+    }
+  
+    if (attractionsSiteLoading) {
+      return (
+        <h1 className='text-2xl text-white font-extrabold text-center'>
+          Loading...
+        </h1>
+      );
+    }
+  
+    if (attractionsSiteError) {
+      return (
+        <ErrorComponent
+          errortext={attractionsSiteError}
+        />
+      );
+    }
+
+  return (
+    <div className='border-slate-300 border-2 rounded-md p-2 bg-slate-400 my-2'>
+      <h3 className='text-center text-gray-200 font-extrabold underline'>
+        Top Attractions
+      </h3>
+      <div className='border-slate-300 border-2 rounded-md p-2 bg-slate-400 my-2 '>
+        <ul className='flex justify-between '>
+          <li className='text-sm font-semibold text-gray-300'>One</li>
+          <li className='text-sm font-semibold text-gray-300'>num</li>
+        </ul>
+        <ul className='flex justify-between '>
+          <li className='text-sm font-semibold text-gray-300'>One</li>
+          <li className='text-sm font-semibold text-gray-300'>num</li>
+        </ul>
+        <ul className='flex justify-between '>
+          <li className='text-sm font-semibold text-gray-300'>One</li>
+          <li className='text-sm font-semibold text-gray-300'>num</li>
+        </ul>
+        <ul className='flex justify-between '>
+          <li className='text-sm font-semibold text-gray-300'>One</li>
+          <li className='text-sm font-semibold text-gray-300'>num</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
