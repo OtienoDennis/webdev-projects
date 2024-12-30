f89d7a38eafaa766cc9e5ddd51591b72c05323d6
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-30 21:56:35 +0300

    Refactor CardInformation component for improved layout and styling consistency

--- frontend-projects/travel-planner-app/src/components/CardInformation.jsx
+++ frontend-projects/travel-planner-app/src/components/CardInformation.jsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom';
 
-export default function CardInformation({ city, cityCode , country, latitude, longitude}) {
-
+export default function CardInformation({
+  city,
+  cityCode,
+  country,
+  latitude,
+  longitude,
+}) {
   return (
     <Link to={`/destination/${city}/${cityCode}/${latitude}/${longitude}`}>
-      <div className='flex justify-between mt-3 bg-slate-400 p-2 rounded hover:scale-105 hover:cursor-pointer transition ease-in-out duration-[25]'>
-        <p className='text-white text-sm'>{city}</p>
-        <p className='text-white text-sm'>{country}</p>
+      <div className='flex sm:flex-row justify-between items-center mt-3 bg-slate-400 p-3 sm:p-4 rounded hover:scale-105 hover:cursor-pointer transition ease-in-out duration-200 w-11/12 m-auto'>
+        <p className='text-white text-sm sm:text-base md:text-lg font-medium'>
+          {city}
+        </p>
+        <p className='text-white text-sm sm:text-base md:text-lg font-medium'>
+          {country}
+        </p>
       </div>
     </Link>
   );
