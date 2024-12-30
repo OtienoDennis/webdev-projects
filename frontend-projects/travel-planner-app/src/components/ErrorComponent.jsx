c877acf1443ca43db2c9a58f575807220a345b6a
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-12-30 18:29:14 +0300

    Update ErrorComponent styles for improved visibility and aesthetics

--- frontend-projects/travel-planner-app/src/components/ErrorComponent.jsx
+++ frontend-projects/travel-planner-app/src/components/ErrorComponent.jsx
@@ -1,6 +1,6 @@
 export default function ErrorComponent({ errortext }) {
   return (
-    <div className='border-slate-300 border-2 rounded-md p-2 bg-slate-400 my-2'>
+    <div className='border-slate-300 border-2 rounded-md p-2 bg-gray-50 my-2'>
       <p className='text-2xl text-red-300 font-extrabold text-center'>
         {errortext}!!
       </p>
