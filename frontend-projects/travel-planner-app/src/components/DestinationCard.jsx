41ce519d13318cecbdf7015fc97adf2975e5207c
Dennis Otieno <dennisotieno.dev@gmail.com>
2025-01-02 16:50:02 +0300

    Refactor DestinationCard component layout for improved grid structure and spacing

--- frontend-projects/travel-planner-app/src/components/DestinationCard.jsx
+++ frontend-projects/travel-planner-app/src/components/DestinationCard.jsx
@@ -47,32 +47,32 @@ export default function DestinationCard({
 
   return (
     <div className='mb-5'>
-      <h1 className='text-2xl text-white font-extrabold text-center'>
+      <h1 className='text-white font-extrabold text-center md:text-2xl'>
         Related to the search{' '}
         <span className='text-gray-950 bg-amber-200 py-1 px-2 rounded text-lg'>
           {keyword}
         </span>
       </h1>
-      {citiesArray.map((item, index) => {
-        let city = item['name'];
-        const cityCode = item?.iataCode ?? airportsData;
-        const country = returnCountryName(item?.address.countryCode);
-        const latitude = item?.geoCode?.latitude;
-        const longitude = item?.geoCode?.longitude;
-        return (
-          <div
-            key={index}
-            className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
-            <CardInformation
-              city={city}
-              cityCode={cityCode}
-              latitude={latitude}
-              longitude={longitude}
-              country={country}
-            />
-          </div>
-        );
-      })}
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+        {citiesArray.map((item, index) => {
+          let city = item['name'];
+          const cityCode = item?.iataCode ?? airportsData;
+          const country = returnCountryName(item?.address.countryCode);
+          const latitude = item?.geoCode?.latitude;
+          const longitude = item?.geoCode?.longitude;
+          return (
+            <div key={index}>
+              <CardInformation
+                city={city}
+                cityCode={cityCode}
+                latitude={latitude}
+                longitude={longitude}
+                country={country}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
