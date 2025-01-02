c39f8815541c292e34fc537a4639c6517fc94e0f
Dennis Otieno <dennisotieno.dev@gmail.com>
2025-01-02 16:49:56 +0300

    Refactor DestinationInformation component layout for improved responsiveness and organization

--- frontend-projects/travel-planner-app/src/components/DestinationInformation.jsx
+++ frontend-projects/travel-planner-app/src/components/DestinationInformation.jsx
@@ -8,25 +8,35 @@ export default function DestinationInformation({ dispatch, myCityCode }) {
   const { city, cityCode, latitude, longitude } = useParams();
 
   return (
-    <div className='rounded-lg py-4 px-2 my-3 bg-slate-500 shadow-lg shadow-white w-11/12 m-auto'>
-      <FlightOffers
-        cityCode={cityCode}
-        dispatch={dispatch}
-        myCityCode={myCityCode}
-      />
-      <HotelAccomodations
-        cityCode={cityCode}
-        dispatch={dispatch}
-        city={city}
-        latitude={latitude}
-        longitude={longitude}
-      />
-      <TopAttractions
-        dispatch={dispatch}
-        latitude={latitude}
-        longitude={longitude}
-      />
-      <WeatherForecast city={city} dispatch={dispatch} />
+    <div className='rounded-lg py-4 px-2 my-3 bg-slate-500 shadow-lg shadow-white w-11/12 m-auto overflow-y-auto'>
+      <div className='md:grid lg:grid md:grid-cols-2 gap-4 lg:grid-cols-2'>
+        <div className='h-screen overflow-y-auto'>
+          <FlightOffers
+            cityCode={cityCode}
+            dispatch={dispatch}
+            myCityCode={myCityCode}
+          />
+        </div>
+        <div className='h-screen overflow-y-auto'>
+          <HotelAccomodations
+            cityCode={cityCode}
+            dispatch={dispatch}
+            city={city}
+            latitude={latitude}
+            longitude={longitude}
+          />
+        </div>
+        <div className='md:col-span-2'>
+          <TopAttractions
+            dispatch={dispatch}
+            latitude={latitude}
+            longitude={longitude}
+          />
+        </div>
+        <div className='md:col-span-2'>
+          <WeatherForecast city={city} dispatch={dispatch} />
+        </div>
+      </div>
     </div>
   );
 }
