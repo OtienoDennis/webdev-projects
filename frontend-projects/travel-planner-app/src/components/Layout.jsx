9f0b5ce1013bdf1f879e32de632bd71e1110d207
Dennis Otieno <dennisotieno.dev@gmail.com>
2025-01-02 17:20:38 +0300

    Update Layout component header styling for improved visual hierarchy and color contrast

--- frontend-projects/travel-planner-app/src/components/Layout.jsx
+++ frontend-projects/travel-planner-app/src/components/Layout.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom';
 export default function Layout() {
   return (
     <div>
-      <h1 className='text-blue-900 text-center pt-8 text-3xl font-bold font-sans'>
-        TRAVEL PLANNER
+      <h1 className='text-[#333] text-center text-xl pt-8 md:text-3xl font-extrabold font-sans'>
+        TRAVEL <span className='text-[#ca1a37]'>PLANNER</span>
       </h1>
       <Outlet />
     </div>
