f4753336e228c9eb6d8e2167ce42b6101deff2bc
Dennis Otieno <dennisotieno.dev@gmail.com>
2025-01-03 03:11:51 +0300

    Enhance Hotel component styling and improve modal interaction

--- frontend-projects/travel-planner-app/src/components/particularDestination/hotelModal/Hotel.jsx
+++ frontend-projects/travel-planner-app/src/components/particularDestination/hotelModal/Hotel.jsx
@@ -8,12 +8,12 @@ export default function Hotel({
 }) {
   return (
     <div
-      className='border-slate-300 border-2 rounded-md p-2 bg-slate-400 my-3 bg-opacity-50'
+      className='border-slate-300 border-2 rounded-md p-2 bg-slate-400 my-3 bg-opacity-50 active:bg-opacity-90'
       onClick={() => {
         setOneHotelId(hotelId);
         openModal();
       }}>
-      <ul className='flex justify-between gap-16 hover:font-extrabold hover:cursor-pointer transition ease-in-out duration-200'>
+      <ul className='flex justify-between gap-16 hover:font-extrabold hover:cursor-pointer transition ease-in-out duration-200 '>
         <li className='text-md'>{hotelName}</li>
         <li className='text-md'>{`${distance} KM FROM ${city} city.`}</li>
       </ul>
