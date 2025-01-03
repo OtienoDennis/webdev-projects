ba4734fffa169d17d8676e1767bbb11484d35db6
Dennis Otieno <dennisotieno.dev@gmail.com>
2025-01-03 03:11:29 +0300

    Add itinerary creation and editing functionality to ItineraryPlanner component

--- frontend-projects/travel-planner-app/src/components/ItineraryPlanner.jsx
+++ frontend-projects/travel-planner-app/src/components/ItineraryPlanner.jsx
@@ -1,52 +1,140 @@
+import { useState } from 'react';
 import Button from './Button';
+import { Link } from 'react-router-dom';
+import ItineraryModal from './ItineraryModal';
 
-export default function ItineraryPlanner({
-  itineraryInformation,
-  onEdit,
-  onSave,
-  onDelete,
-}) {
+export default function ItineraryPlanner({ itineraryInformation, dispatch }) {
+  const [savedState, setSavedState] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [valuesToEdit, setValuesToEdit] = useState({});
+  const [editedIndex, setEditedIndex] = useState(null);
+  function onEdit(index) {
+    setIsModalOpen(true);
+    setSavedState(false);
+    setEditedIndex(index);
+    setValuesToEdit(itineraryInformation[index]);
+  }
+
+  function onDelete(index) {
+    let filteredItineraryInformation = itineraryInformation.filter(
+      (item, idn) => {
+        return idn !== index;
+      }
+    );
+    dispatch({
+      type: 'itineraryInformation',
+      payload: filteredItineraryInformation,
+    });
+  }
+
+  function onCreate() {
+    setIsModalOpen(true);
+    setSavedState(false);
+    setValuesToEdit(null);
+    if (itineraryInformation.length < 1) {
+      setEditedIndex(0);
+    } else {
+      setEditedIndex(itineraryInformation.length);
+    }
+  }
+
+  if (itineraryInformation.length < 1) {
+    return (
+      <div className='flex flex-col items-center h-screen gap-10 mt-5'>
+        <div className=''>
+          <Link to='/'>
+            <p className='text-white font-extrabold text-center text-xl bg-amber-500 p-2 rounded md:text-2xl hover:bg-opacity-90 hover:cursor-pointer active:scale-95 transition ease-in-out duration-200 w-max m-auto'>
+              SEARCH FOR A DESTINATION TO CREATE ITINERARY
+            </p>
+          </Link>
+        </div>
+
+        <Button
+          onClick={onCreate}
+          className={
+            'text-white text-xl font-semibold bg-green-700 p-2 rounded-md hover:text-slate-300 hover:cursor-pointer transition ease-in-out duration-200 active:scale-95'
+          }>
+          ADD ITINERARY
+        </Button>
+
+        {isModalOpen && (
+          <ItineraryModal
+            itineraryInformation={itineraryInformation}
+            valuesToEdit={valuesToEdit}
+            setIsModalOpen={setIsModalOpen}
+            dispatch={dispatch}
+            editedIndex={editedIndex}
+            setSavedState={setSavedState}
+          />
+        )}
+      </div>
+    );
+  }
   return (
-    <div className='flex flex-col items-center gap-5'>
-      <h1 className='text-white text-5xl font-extrabold p-5 text-center'>
-        Visit to {itineraryInformation?.city} City!
-      </h1>
-      <div className='text-center text-white text-3xl p-5 border-2 border-white rounded-md bg-slate-400 bg-opacity-50 font-semibold'>
-        <p>FlightDate: {itineraryInformation?.flightDate}</p>
-        <p>Flight Time: {itineraryInformation?.flightTime} hrs</p>
-        <p>
-          Air Fare: {itineraryInformation?.airFare}{' '}
-          {itineraryInformation?.currency}
-        </p>
-        <p>
-          Explore {itineraryInformation?.attractionToVisit} in{' '}
-          {itineraryInformation?.city} city!
-        </p>
-      </div>
-      <div className='gap-5 mt-5 flex'>
-        <Button
-          onClick={onEdit}
-          className={
-            'text-white text-xl font-semibold bg-stone-500 p-2 rounded-md hover:text-slate-300 hover:cursor-pointer transition ease-in-out duration-200 active:scale-95'
-          }>
-          EDIT
-        </Button>
+    <div>
+      <Button
+        onClick={onCreate}
+        className={
+          'text-white text-xl font-semibold bg-green-700 p-2 rounded-md hover:text-slate-300 hover:cursor-pointer transition ease-in-out duration-200 active:scale-95 fixed left-12 top-28'
+        }>
+        ADD ITINERARY
+      </Button>
+      <div className='bg-slate-400 mt-5 p-3 rounded-lg bg-opacity-50 relative'>
+        {itineraryInformation.map((item, index) => {
+          return (
+            <div key={index}>
+              <div className='flex flex-col items-center gap-5'>
+                <h1 className='text-white text-5xl font-extrabold p-5 text-center'>
+                  Visit {item.city} City!
+                </h1>
+                <div className='text-center text-white text-3xl p-5 border-2 border-white rounded-md bg-slate-400 bg-opacity-50 font-semibold'>
+                  <p>FlightDate: {item.flightDate}</p>
+                  <p>Flight Time: {item.flightTime} hrs</p>
+                  <p>
+                    Air Fare: {item.airFare} {item.currency}
+                  </p>
+                  <p>
+                    Explore {item.attractionToVisit} in {item.city} city!
+                  </p>
 
-        <Button
-          onClick={onDelete}
-          className={
-            'text-white text-xl font-semibold bg-red-600 p-2 rounded-md hover:text-slate-300 hover:cursor-pointer transition ease-in-out duration-200 active:scale-95'
-          }>
-          DELETE
-        </Button>
-        <Button
-          onClick={onSave}
-          className={
-            'text-white text-xl font-semibold bg-green-400 p-2 rounded-md hover:text-slate-300 hover:cursor-pointer transition ease-in-out duration-200 active:scale-95'
-          }>
-          SAVE
-        </Button>
+                  <div className='gap-5 mt-5 flex justify-center'>
+                    {savedState && (
+                      <Button
+                        onClick={() => onEdit(index)}
+                        className={
+                          'text-white text-xl font-semibold bg-stone-500 p-2 rounded-md hover:text-slate-300 hover:cursor-pointer transition ease-in-out duration-200 active:scale-95'
+                        }>
+                        EDIT
+                      </Button>
+                    )}
+
+                    <Button
+                      onClick={() => onDelete(index)}
+                      className={
+                        'text-white text-xl font-semibold bg-red-600 p-2 rounded-md hover:text-slate-300 hover:cursor-pointer transition ease-in-out duration-200 active:scale-95'
+                      }>
+                      DELETE
+                    </Button>
+                    {/* {!savedState && (
+                      
+                    )} */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
+      {isModalOpen && (
+        <ItineraryModal
+          itineraryInformation={itineraryInformation}
+          valuesToEdit={valuesToEdit}
+          setIsModalOpen={setIsModalOpen}
+          dispatch={dispatch}
+          editedIndex={editedIndex}
+          setSavedState={setSavedState}
+        />
+      )}
     </div>
   );
 }
