969b3a560674e1860496f25c61e509aa890245be
OtienoDennis <dennisotieno.dev@gmail.com>
2025-02-15 22:00:39 +0300

    Clear default email and password fields in LoginForm for improved user experience

--- frontend-projects/the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ frontend-projects/the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('denoti98@gmail.com');
-  const [password, setPassword] = useState('pass2025');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const { login, isLoading } = useLogin();
 
