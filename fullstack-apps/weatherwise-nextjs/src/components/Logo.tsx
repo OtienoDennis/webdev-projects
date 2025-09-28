e674a61d99aecd3fa143dab1b124ea1e378d7f5f
OtienoDennis <dennisotieno.dev@gmail.com>
2025-09-28 21:59:24 +0300

    Changes

---
+++ fullstack-apps/weatherwise-nextjs/src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+function Logo() {
+  return (
+    <div>
+    <Image className=''></Image>
+    </div>
+  )
+}
+
+export default Logo
---
+++ fullstack-apps/weatherwise-nextjs/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+function NavBar() {
+  return (
+    <div>
+      <ul className='md:flex md:gap-3'>
+        <li>Home</li>
+        <li>Blog</li>
+        <li>Services</li>
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
