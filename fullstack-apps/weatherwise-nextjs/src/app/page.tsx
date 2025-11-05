1a9d5347b5de4c2d15c71e3264b7923c6a95f249
OtienoDennis <dennisotieno.dev@gmail.com>
2025-11-05 23:46:28 +0300

    Adds advert component and updates layout styling
    Introduces an advert section to enhance user engagement and updates the layout to use a grid for improved responsiveness and alignment across devices.

--- fullstack-apps/weatherwise-nextjs/src/app/page.tsx
+++ fullstack-apps/weatherwise-nextjs/src/app/page.tsx
@@ -2,21 +2,24 @@ import NavBar from '@/components/NavBar';
 import DashboardUI from './dashboard/DashboardUI';
 import OtherCities from '@/components/OtherCities';
 import Contacts from '@/components/Contacts';
+import Advert from '@/components/Advert';
 
 export default function Home() {
   return (
-    <div className='bg-[#BFD7FF] h-screen p-3 md:px-10 overflow-auto relative'>
+    <div className='bg-[#BFD7FF] h-screen p-3 md:px-10 overflow-auto relative text-black'>
       <h1 className='font-bold text-xl md:text-3xl text-center p-2'>
         Weather Wise App
       </h1>
       <NavBar />
       <DashboardUI />
-      <div className='flex justify-between md:flex-col md:gap-5 md:fixed md:top-60 md:left-100'>
+      <div className='grid gap-3 grid-cols-3 md:grid-rows-3 md:grid-cols-none md:absolute md:top-[230px] md:left-[25.5vw] md:h-[65.5vh] mt-4 md:mt-0'>
         <OtherCities />
         <OtherCities />
         <OtherCities />
       </div>
+      <Advert />
       <Contacts />
     </div>
   );
 }
+
