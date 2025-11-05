abfaa37e9662607a9849c9dc79b572f42d7b8956
OtienoDennis <dennisotieno.dev@gmail.com>
2025-11-05 23:47:19 +0300

    Refines layout for responsive city panel
    Adjusts height and width classes to improve display across devices, ensuring more consistent sizing and alignment in both mobile and desktop views.

--- fullstack-apps/weatherwise-nextjs/src/components/OtherCities.tsx
+++ fullstack-apps/weatherwise-nextjs/src/components/OtherCities.tsx
@@ -1,7 +1,4 @@
 export default function OtherCities() {
-  return (
-    <div className='w-[30vw] md:w-[21vw] h-[15vh] rounded-lg bg-[#dff0ff] m-2.5 md:m-0 p-2'>
-      OtherCities
-    </div>
-  );
+  return <div className='h-[12vh] md:h-[100%] md:w-[20.5vw] rounded-lg bg-[#dff0ff] p-2'>OtherCities</div>;
 }
+
