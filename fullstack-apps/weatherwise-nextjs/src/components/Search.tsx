006cf54857d6f49c4177b7e8d739cd5fcc6224e2
OtienoDennis <dennisotieno.dev@gmail.com>
2025-11-05 23:47:42 +0300

    Improves search layout for better alignment and sizing
    Adjusts the flex layout and input/button sizing to achieve more balanced spacing and alignment across screen sizes. Enhances visual consistency on medium and larger displays.

--- fullstack-apps/weatherwise-nextjs/src/components/Search.tsx
+++ fullstack-apps/weatherwise-nextjs/src/components/Search.tsx
@@ -1,8 +1,8 @@
 export default function Search() {
   return (
-    <div className='flex gap-3 w-[100%] md:w-[50vw] md:content-start'>
+    <div className='flex justify-between gap-3 w-[100%] md:w-[44vw]'>
       <input
-        className='bg-[#e3e4e6] text-2xl p-3 w-2/3 rounded-2xl h-11 focus:outline-none'
+        className='bg-[#e3e4e6] text-2xl p-3 w-3/3 rounded-2xl h-11 focus:outline-none'
         placeholder='Enter text here'
       />
       <button className='font-extrabold text-3xl bg-[#EAA221] p-1 rounded-lg cursor-pointer text-white'>
