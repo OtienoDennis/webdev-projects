16e5b80e1a923d163e5e737c8b7a2b2670fb1a49
OtienoDennis <dennisotieno.dev@gmail.com>
2025-11-05 23:47:29 +0300

    Removes redundant margin utility from forecast container
    Eliminates an unnecessary margin class to improve layout consistency,
    likely aligning the component's spacing with the surrounding design or
    preventing unwanted extra space on certain viewports.

--- fullstack-apps/weatherwise-nextjs/src/components/OtherForecast.tsx
+++ fullstack-apps/weatherwise-nextjs/src/components/OtherForecast.tsx
@@ -1,6 +1,6 @@
 export default function OtherForecast() {
   return (
-    <div className='w-[21vw] md:w-[100%] h-[15vh] rounded-lg bg-[#dff0ff] m-2.5 md:m-0 p-2'>
+    <div className='w-[21vw] md:w-[100%] h-[15vh] rounded-lg bg-[#dff0ff] p-2'>
       Other
     </div>
   );
