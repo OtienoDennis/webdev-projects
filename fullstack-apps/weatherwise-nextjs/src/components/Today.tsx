f292300d53031e3693fbd21c2c073eb60cd1eb0f
OtienoDennis <dennisotieno.dev@gmail.com>
2025-12-17 23:32:11 +0300

    Refactors Today component to display current date and weather information structure

--- fullstack-apps/weatherwise-nextjs/src/components/Today.tsx
+++ fullstack-apps/weatherwise-nextjs/src/components/Today.tsx
@@ -1,3 +1,19 @@
 export default function Today() {
-  return <div className='rounded-lg bg-[#F0F8FF] md:h-[25vh] p-2'>Today</div>;
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const currentDay = `${year}-${month}-${day}`;
+  console.log(currentDay);
+  return (
+    <div className='rounded-lg bg-[#F0F8FF] md:h-[40vh] p-2'>
+      <p>Day:</p>
+      <p>Temperature: </p>
+      <p>Condition:</p>
+      <p>Weather Icon:</p>
+      <p>Humidity:</p>
+    </div>
+  );
 }
+
+
