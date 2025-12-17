f5d93c55e87a2eeb10ce04972396dd390fa7a81d
OtienoDennis <dennisotieno.dev@gmail.com>
2025-12-17 23:29:07 +0300

    Refactors DashboardUI layout and removes redundant OtherForecast components

--- fullstack-apps/weatherwise-nextjs/src/app/dashboard/DashboardUI.tsx
+++ fullstack-apps/weatherwise-nextjs/src/app/dashboard/DashboardUI.tsx
@@ -3,16 +3,13 @@ import Search from '@/components/Search';
 import Today from '@/components/Today';
 
 export default function DashboardUI() {
+
   return (
-    <div className='mt-5 md:flex md:justify-between md:items-start'>
+    <div className='md:mt-5 md:flex md:justify-between md:items-start'>
       <Search />
-      <div className='grid justify-between grid-cols-2 mt-5 gap-5 md:flex md:flex-col md:w-[44vw] md:mt-0'>
+      <div className='grid justify-between grid-rows-2 mt-5 gap-5 md:flex md:flex-col md:w-[44vw] md:mt-0'>
         <Today />
         <div className='grid grid-cols-2 gap-2'>
-          <OtherForecast />
-          <OtherForecast />
-          <OtherForecast />
-          <OtherForecast />
           <OtherForecast />
           <OtherForecast />
         </div>
@@ -22,5 +19,3 @@ export default function DashboardUI() {
     </div>
   );
 }
-
-// md:flex md:flex-col
