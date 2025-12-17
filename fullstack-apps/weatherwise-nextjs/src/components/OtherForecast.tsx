5ce55393bb8b623add7f0be18fb6501a962b45df
OtienoDennis <dennisotieno.dev@gmail.com>
2025-12-17 23:31:33 +0300

    Updates OtherForecast component layout for improved structure and responsiveness

--- fullstack-apps/weatherwise-nextjs/src/components/OtherForecast.tsx
+++ fullstack-apps/weatherwise-nextjs/src/components/OtherForecast.tsx
@@ -1,7 +1,10 @@
 export default function OtherForecast() {
   return (
-    <div className='w-[21vw] md:w-[100%] h-[15vh] rounded-lg bg-[#dff0ff] p-2'>
-      Other
+    <div className='w-[46vw] md:w-[100%] h-[15vh] md:h-[30vh] rounded-lg bg-[#dff0ff] p-2'>
+      <p>Day:</p>
+      <p>Temparature:</p>
+      <p>Weather Icon:</p>
+      <p>Weather Icon:</p>
     </div>
   );
 }
